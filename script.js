3e50d1b6756b306bf4503396db4e9a320391337f
marwankhalaf <114322450+marwankhalaf@users.noreply.github.com>
2022-09-25 05:09:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let screen = document.getElementById("screen");
+let equal = document.getElementById("equl");
+let buttons = Array.from(document.getElementsByClassName("button")) ;
+
+console.log(screen)
+console.log(equal)
+console.log(buttons)
+
+buttons.map(button =>{
+   button.addEventListener("click" , (e)=>{
+         switch (e.target.innerText){
+            case "C" :
+            screen.value = ""
+            break;
+            case "←" :
+            screen.value = screen.value.slice(0 , -1)
+            break;
+            case "=" :
+            try {
+               screen.value = eval(screen.value)
+            }catch{
+               alert("invalid")
+            }
+            break;
+            default :
+            screen.value += e.target.innerText  
+         }
+   })
+
+})
